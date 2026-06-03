e1fb9974be6afba7ecee4c4745d7931222ff9f29
Kauan Lima <kauan.lima8@aluno.cps.sp.gov.br>
2026-06-02 22:57:00 -0300

    feature: criação da página games

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,15 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    loadComponent: () => import('./pages/home.page').then((m) => m.HomePage),
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
+  },
+  {
+    path: 'games',
+    loadComponent: () => import('./pages/games/games.page').then( m => m.GamesPage)
   },
 ];
