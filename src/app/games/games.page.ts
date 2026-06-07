c6670657f9077306e0ebf3bb128a19f6e77890be
kaiquebaccas <kaiquebaccas02@gmail.com>
2026-06-07 16:15:48 -0300

    feat: mostra mensagem de erro em caso de falha da API

--- src/app/games/games.page.ts
+++ src/app/games/games.page.ts
@@ -5,11 +5,15 @@ import {
   IonTitle,
   IonContent,
   IonCard,
+  IonCardContent,
   IonInfiniteScroll,
   IonInfiniteScrollContent,
+  IonSkeletonText,
+  IonText,
 } from '@ionic/angular/standalone';
 import { Rawg } from '../services/rawg';
 import { CommonModule } from '@angular/common';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-games',
@@ -23,35 +27,73 @@ import { CommonModule } from '@angular/common';
     IonTitle,
     IonContent,
     IonCard,
+    IonCardContent,
     IonInfiniteScroll,
     IonInfiniteScrollContent,
+    IonSkeletonText,
+    IonText,
   ],
 })
 export class GamesPage implements OnInit {
   games: any[] = [];
   page = 1;
+  loading = true;
+  errorMessage = '';
+  hasError = false;
 
-  constructor(private rawg: Rawg) {}
+  constructor(
+    private rawg: Rawg,
+    private alertController: AlertController,
+  ) {}
 
   ngOnInit() {
     this.loadGames();
   }
   loadGames() {
-    this.rawg.getGames(this.page).subscribe((data: any) => {
-      this.games = [...this.games, ...data.results];
-      console.log(this.games);
-      console.log(this.games.length);
-    });
+    this.rawg.getGames(this.page).subscribe(
+      (data: any) => {
+        this.games = [...this.games, ...data.results];
+        this.loading = false;
+      },
+      async (error) => {
+        console.error(error);
+        this.loading = false;
+        this.hasError = true;
+        await this.showErrorAlert(
+          'Não foi possível carregar os jogos. Por favor, tente novamente mais tarde.',
+        );
+      },
+    );
   }
 
   loadMore(event: any) {
     this.page++;
 
-    this.rawg.getGames(this.page).subscribe((data: any) => {
-      this.games = [...this.games, ...data.results];
-      console.log(this.games);
-      console.log(this.games.length);
-      event.target.complete();
+    this.rawg.getGames(this.page).subscribe(
+      (data: any) => {
+        this.games = [...this.games, ...data.results];
+        event.target.complete();
+      },
+      async (error) => {
+        console.error(error);
+        this.hasError = true;
+        await this.showErrorAlert(
+          'Não foi possível carregar os jogos. Por favor, tente novamente mais tarde.',
+        );
+        event.target.complete();
+      },
+    );
+  }
+
+  async showErrorAlert(message: string) {
+    const alert = await this.alertController.create({
+      header: 'Erro',
+      message,
+      buttons: [
+        { text: 'OK', role: 'cancel' },
+        { text: 'Tentar novamente', handler: () => this.loadGames() },
+      ],
     });
+    await alert.present();
   }
 }
